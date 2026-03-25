6c8fa0a27f5623151449e4061cc107c1ae01bae3
John Harrington <john.harrington@lr.org>
2026-03-25 20:26:11 +0000

    Remove builtin nmea2000 lib

--- pypilot/nmea2000/__init__.py
+++
@@ -1,7 +0,0 @@
-from .decoder import NMEA2000Decoder
-from .encoder import NMEA2000Encoder
-from .consts import FieldTypes, PhysicalQuantities, ManufacturerCodes
-from .message import NMEA2000Message, NMEA2000Field, IsoName
-from .ioclient import EByteNmea2000Gateway, ActisenseNmea2000Gateway, YachtDevicesNmea2000Gateway, WaveShareNmea2000Gateway, PythonCanAsyncIOClient, AsyncIOClient, State
-
-__all__ = ["NMEA2000Decoder", "NMEA2000Encoder", "NMEA2000Message", "NMEA2000Field", "EByteNmea2000Gateway", "ActisenseNmea2000Gateway", "YachtDevicesNmea2000Gateway", "WaveShareNmea2000Gateway", "PythonCanAsyncIOClient", "AsyncIOClient", "FieldTypes", "PhysicalQuantities", "State", "IsoName", "ManufacturerCodes"]
